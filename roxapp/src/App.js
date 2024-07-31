b79bc8458efaf652bd9b74e955064ad35f8a53d2
xsergeynik <xsergeynik@gmail.com>
2024-07-31 20:33:42 +0300

    begin dev cooxApp

--- roxapp/src/App.js
+++ roxapp/src/App.js
@@ -1,28 +1,12 @@
-  import logo from './logo.svg';
   import './App.css';
-  import Main from './components/Main/Main.js'
+  import RecipeList from './components/recipe/RecipeList';
 
   function App() {
     return (
       <div className="App">
 
-      <div className="App-block">
-        <Main/>
-        </div>  
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+<RecipeList/>
+        
       </div>
     );
   }
