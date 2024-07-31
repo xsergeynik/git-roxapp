ae9da726410514e54ace90e8556c8ca750bd9d99
xsergeynik <xsergeynik@gmail.com>
2024-07-31 09:17:22 +0300

    first test

--- roxapp/src/App.js
+++ roxapp/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+    <div className="App-block">Hello world</div>  
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
