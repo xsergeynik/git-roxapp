b59099e1cbfdd766479d827fc6b41b26886926fe
xsergeynik <xsergeynik@gmail.com>
2024-07-31 15:17:50 +0300

    components add

---
+++ roxapp/src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainHeader from './MainHeader';
+
+function Main(props) {
+    return (
+        <div>
+            <MainHeader title={'Serhii'} phone={'63583999'} id/>
+        </div>
+    );
+}
+
+export default Main;
