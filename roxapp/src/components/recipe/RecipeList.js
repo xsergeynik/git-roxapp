64acae31aca9d0ccceef53cb0a2778bd182fbc83
Sergey <xsergeynik@gmail.com>
2024-08-01 23:30:26 +0300

    style componets

--- roxapp/src/components/recipe/RecipeList.js
+++ roxapp/src/components/recipe/RecipeList.js
@@ -1,39 +1,44 @@
 import React from "react";
-import recipesAll from "../../backend/recipesAll";
+import recipesAll from "../../backend/recipesAll"; // Assuming recipesAll is an array of recipe objects
 
 const RecipeList = () => {
-  return (
-    <>
-    hello
-    <ul className="recipe">
-      {recipesAll.map((recipe) => (
-        <li key={recipe.id} className="recipe__item">
-          <h3 className="recipe__title">{recipe.name}</h3>
-          <p className="recipe__description">{recipe.description}</p>
+	return (
+		<>
+			<section className="recipe">
+				<div className="recipe__container container">
+					<ul className="recipe__list">
+						{recipesAll.map((recipe) => (
+							<li key={recipe.id} className="recipe__item">
+								<div className="recipe__image">
+									<img src={recipe.image} alt={recipe.name} />
+								</div>
+								<h3 className="recipe__title">{recipe.name}</h3>
+								<p className="recipe__description">{recipe.description}</p>
 
-          <ul>
-            {recipe.instructions.map((instruction, index) => (
-              <li key={index}>
-                {index + 1} {instruction}
-              </li>
-            ))}
-          </ul>
+								{/* Instructions */}
+								<ul className="recipe__instructions">
+									{recipe.instructions.map((instruction, index) => (
+										<li key={index}>
+											{index + 1}. {instruction.description}
+										</li>
+									))}
+								</ul>
 
-          {/* <ul>
-            {recipe.ingredients.map((ingredient, index) => (
-              <li key={index}>
-                {ingredient.name}
-                {ingredient.quantity}
-                {ingredient.unit}
-              </li>
-            ))}
-          </ul> */}
-
-        </li>
-      ))}
-      </ul>
-      </>
-  );
+								{/* Ingredients */}
+								<ul className="recipe__ingredients">
+									{recipe.ingredients.map((ingredient, index) => (
+										<li key={index}>
+											{ingredient.name}, {ingredient.quantity}, {ingredient.unit}
+										</li>
+									))}
+								</ul>
+							</li>
+						))}
+					</ul>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default RecipeList;
